77141dd210db74a541d6a25b849c722ea1159e0a
wzr521135 <37637688@qq.com>
2019-12-09 21:16:51 +0800

    Initial commit

---
+++ src/main/java/socket/ClientSocket.java
@@ -0,0 +1,9 @@
+package socket;
+
+/**
+ * @auther wzr
+ * 创建时间: 2019-12-08 20:15
+ * @Description:
+ */
+public class ClientSocket {
+}
---
+++ src/main/java/socket/SocketServerTest.java
@@ -0,0 +1,25 @@
+package socket;
+
+import java.net.ServerSocket;
+
+/**
+ * @auther wzr
+ * 创建时间: 2019-12-08 20:07
+ * @Description:
+ */
+public class SocketServerTest {
+
+    public static void main(String[] args) throws  Exception{
+
+         //初始化服务端socket并且绑定99999端口
+        ServerSocket serverSocket=new ServerSocket(9999);
+
+
+
+
+    }
+
+
+
+
+}
